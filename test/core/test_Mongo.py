5bf8b0c0a2776f975fa75b9d3954b4fecfd743d2
Gilles Degols <gilles@degols.net>
2018-05-26 18:01:23 +0200

    Add test to create generic file

--- test/core/test_Mongo.py
+++ test/core/test_Mongo.py
@@ -1,5 +1,7 @@
 import unittest
 import json
+from bson import json_util
+
 from src.core.Configuration import Configuration
 from src.core.Mongo import Mongo
 from src.core.GenericFile import GenericFile
@@ -11,10 +13,29 @@ class TestMongo(unittest.TestCase):
     def setUp(self):
         Configuration.FILEPATH = 'test/resources/conf/mongofs.json'
         self.obj = Mongo()
-        self.obj.clean_database()
+
+        # To ease some tests
+        with open('test/resources/data/file.json','r') as f:
+            self.file_raw = json_util.loads(f.read())
+            del self.file_raw['_id']
+            del self.file_raw['uploadDate']
+        self.file = self.obj.load_generic_file(self.file_raw)
+
+        with open('test/resources/data/directory.json','r') as f:
+            self.directory_raw = json_util.loads(f.read())
+            del self.directory_raw['_id']
+            del self.directory_raw['uploadDate']
+        self.directory = self.obj.load_generic_file(self.directory_raw)
+
+        with open('test/resources/data/symbolic-link.json','r') as f:
+            self.symbolic_link_raw = json_util.loads(f.read())
+            del self.symbolic_link_raw['_id']
+            del self.symbolic_link_raw['uploadDate']
+        self.symbolic_link = self.obj.load_generic_file(self.symbolic_link_raw)
+
 
     def tearDown(self):
-        pass
+        self.obj.clean_database()
 
     def test_connect(self):
         # Normally the "setUp" should have already created a connection
@@ -48,6 +69,21 @@ class TestMongo(unittest.TestCase):
         pid = self.obj.current_user()['pid']
         expected_lock = filename+';'+str(pid)+';'+hostname
         self.assertEqual(self.obj.lock_id(filename=filename), expected_lock)
+
+    def test_master_lock_id(self):
+        filename = 'test-file'
+        hostname = Mongo.configuration.hostname()
+        pid = 0
+        expected_lock = filename + ';' + str(pid) + ';' + hostname
+        self.assertEqual(self.obj.master_lock_id(filename=filename), expected_lock)
+
+    def test_create_generic_file(self):
+        self.obj.create_generic_file(generic_file=self.file)
+        gf = self.obj.files_coll.find_one({'filename':self.file.filename})
+        del gf['_id']
+        del gf['uploadDate']
+        self.assertEqual(gf, self.file_raw)
+
 
 if __name__ == '__main__':
     unittest.main()
