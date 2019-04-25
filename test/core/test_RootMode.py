87b37a75204f850405e5de26c4f6bf31608d5d59
Vincent Raman <vincent.raman@macq.eu>
2019-04-25 17:25:55 +0200

    Don't forget to force S_IFDIR on root

--- test/core/test_RootMode.py
+++ test/core/test_RootMode.py
@@ -2,6 +2,7 @@ import unittest
 from src.core.Configuration import Configuration
 from src.core.Mongo import Mongo
 from src.core.GenericFile import GenericFile
+from stat import S_IFDIR
 
 class TestRootMode(unittest.TestCase):
     def setUpConfig(self, path):
@@ -19,6 +20,7 @@ class TestRootMode(unittest.TestCase):
         self.assertEqual(GenericFile.configuration.force_root_mode(), False)
         root = self.obj.get_generic_file(filepath='/')
         self.assertEqual(root.metadata['st_mode'] & 0o777, 0o755)
+        self.assertEqual(root.metadata['st_mode'] & S_IFDIR, S_IFDIR)
 
     def test_special_rootMode(self):
         self.setUpConfig('test/resources/conf/mongofs-rootMode777.json')
@@ -26,13 +28,16 @@ class TestRootMode(unittest.TestCase):
         self.assertEqual(GenericFile.configuration.force_root_mode(), True)
         root = self.obj.get_generic_file(filepath='/')
         self.assertEqual(root.metadata['st_mode'] & 0o777, 0o777)
+        self.assertEqual(root.metadata['st_mode'] & S_IFDIR, S_IFDIR)
 
     def test_force_rootMode(self):
         self.setUpConfig('test/resources/conf/mongofs.json')
         root = self.obj.get_generic_file(filepath='/')
         self.assertEqual(root.metadata['st_mode'] & 0o777, 0o755)
+        self.assertEqual(root.metadata['st_mode'] & S_IFDIR, S_IFDIR)
 
         # Now change to force mode to 777, it should update the existing data for /
         self.setUpConfig('test/resources/conf/mongofs-rootMode777.json')
         root = self.obj.get_generic_file(filepath='/')
         self.assertEqual(root.metadata['st_mode'] & 0o777, 0o777)
+        self.assertEqual(root.metadata['st_mode'] & S_IFDIR, S_IFDIR)
